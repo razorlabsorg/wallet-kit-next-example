9d7446e366ca2e56684c7608b0febd30a86c27c6
Nate <89227631+natedotv2@users.noreply.github.com>
2024-01-13 12:49:45 +0100

    Finished app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '@/styles/globals.css'
+import '@razorlabs/wallet-kit/style.css'
+import '../components/App.css';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
